f8bdd3361b68862c0e0b482c5c6cb18931930a00
Jake Drost <Jacob.Drost@daugherty.com>
2016-12-05 16:24:49 -0600

    Removes duplicated dependencies

--- template/model/model.py
+++ template/model/model.py
@@ -26,6 +26,7 @@ def get_template(enable_lombok):
 
 
 def gen_contents(file_info, id_type='Integer'):
+    fields = [Field('id', 'Integer')] + file_info.fields
     dependencies = ''
     body = ''
 
@@ -33,17 +34,19 @@ def gen_contents(file_info, id_type='Integer'):
     for field in file_info.fields:
         body += _field_template.format(type=field.field_type.class_name, name=field.name)
 
+    # Finds all the dependencies needed for the fields
+    for field in fields:
+        if field.field_type.has_dependency() and field.field_type.dependency not in dependencies:
+            dependencies += _dependency_template.format(dep=field.field_type.dependency)
+
     # If lombok is not supported, we must manually
     # enter in the getters and the setters
     if not settings.IS_LOMBOK_SUPPORTED:
-        for field in [Field('id', 'Integer')] + file_info.fields:
+        for field in fields:
             body += _getter_setter_template.format(
                 type=field.field_type.class_name,
                 name=field.name,
                 cap_name=field.name.capitalize())
-
-            if field.field_type.has_dependency():
-                dependencies += _dependency_template.format(dep=field.field_type.dependency)
 
     return template_util.format_template(get_template(settings.IS_LOMBOK_SUPPORTED).format(
         package=file_info.package,
